8e9cdba93e3803d1e51ecf7aa6bef769016e5590
mathcouti <mathcoutipro@gmail.com>
2023-04-24 20:45:49 -0300

    Inserindo jogo da velha e pasta praticas

---
+++ Praticas/Pratica1/Pratica1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+class Program
+{
+    static public void Main(string[] args)
+    {
+        DateTime d1 = DateTime.Now;
+
+        DateTime d2 = d1.AddDays(1);
+
+        Console.WriteLine(d2);
+
+    }
+
+
+}
