18d169f4c76cce261fef126115fb0b1a0ee7de16
mathcouti <mathcoutipro@gmail.com>
2023-05-09 21:16:34 -0300

    adicionado projeto 6 / projeto xadrez

---
+++ Praticas/Pratica6/Entities/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica6.Entities
+{
+    class Account
+    {
+        public int number { get; set; }
+        public string holder { get; set; }
+        public double balance { get; set; }
+        public double withdrawLimit { get; set; }
+
+
+
+    }
+}
